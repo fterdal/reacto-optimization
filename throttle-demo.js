4509af7ef4f5b1010e7348f4c593e32bfc06fcbe
Finn <finnterdal@gmail.com>
2020-02-02 23:57:54 -0600

    [insert intelligible commit message here]

--- throttle-demo.js
+++ throttle-demo.js
@@ -11,10 +11,12 @@ const throttle = (fn, interval = 10) => {
   }
 }
 
-// const throttledConsoleLog = throttle(console.log, 1000)
-// setInterval(() => {
-//   throttledConsoleLog("Hello World")
-//   // console.log("Hello World")
-// }, 10)
+if (require.main === module) {
+  const throttledConsoleLog = throttle(console.log, 1000)
+  setInterval(() => {
+    // throttledConsoleLog("Hello World")
+    console.log("Hello World")
+  }, 10)
+}
 
 module.exports = throttle
